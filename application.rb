1e1ef521efe14b827f5c069706058c0b27a20bcd
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-03 23:10:45 -0400

    add nav bar, working home page, stubbed data for urls

--- application.rb
+++ application.rb
@@ -1,9 +1,13 @@
 require 'bundler'
 Bundler.require(:default, (ENV['RACK_ENV'] || :development).to_sym)
+require 'ostruct'
 
 module My
   class Application < Sinatra::Base
     get '/' do
+      @urls = [
+        ['http://funnyordie.com', 'some site'], ['http://github.com', 'HUB']
+      ].map {|e| OpenStruct.new(name: e[0], description: e[1]) }
       haml :index
     end
   end
