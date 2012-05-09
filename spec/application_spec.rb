9fbacfa067d785a5e951df71c247e98b46cd83bc
Gabriel Horner <gabriel.horner@gmail.com>
2012-05-08 23:44:46 -0400

    add tags to urls page

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -9,8 +9,11 @@ describe My::Application do
     end
 
     it "sees a table with the correct rows" do
-      urls = [['http://funnyordie.com', 'some site'], ['http://github.com', 'HUB']]
-      urls.each {|arr| Url.create name: arr[0], desc: arr[1] }
+      urls = [['http://funnyordie.com', 'some site', 'funny, celebrity'], ['http://github.com', 'HUB', 'code']]
+      urls.each do|arr|
+        Url.create name: arr[0], desc: arr[1]
+        Urls.add_tag arr[0], arr[2].sub(/\s*,\s*/, ',')
+      end
       visit '/'
 
       page.should have_table('last_urls', :rows => urls.reverse)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] = "test"
 ENV['URLS_HOME'] = File.dirname(__FILE__) + '/.urls'
 ENV['URLS_RC'] = ''
+ENV['TAG_HOME'] = File.dirname(__FILE__) + '/.tag'
 
 require File.dirname(__FILE__) + "/../application"
 require 'capybara/rspec'
@@ -14,4 +15,5 @@ Capybara.app = My::Application
 
 at_exit do
   FileUtils.rm_rf(ENV['URLS_HOME'])
+  FileUtils.rm_rf(ENV['TAG_HOME'])
 end
